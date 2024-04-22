46245b6ef9dbcfd603bb201e4906514af040ba9c
mimminou <mimminou@gmail.com>
2024-04-22 20:47:46 +0100

    added swagger docs, accessible from root, Updated README to reflect that

--- back/models/models.go
+++ back/models/models.go
@@ -2,10 +2,17 @@ package models
 
 //Here are the structs that define the models of the DB, needed for unmarshalling/Marhsalling Json
 
+// Book is the schema for a book object
+// @Description Book
 type Book struct {
-	Book_Id   int    `json:"book_id"`
-	Title     string `json:"title"`
-	Author    string `json:"author"`
-	Num_Pages *int   `json:"num_pages,omitempty"`
-	Pub_Date  string `json:"pub_date"`
+	// @Property book_id int true "Book ID"
+	Book_Id int `json:"book_id"`
+	// @Property title string true "Title"
+	Title string `json:"title"`
+	// @Property author string true "Author"
+	Author string `json:"author"`
+	// @Property num_pages string false "Number of pages"
+	Num_Pages *int `json:"num_pages,omitempty"`
+	// @Property pub_date int true "Publication date"
+	Pub_Date string `json:"pub_date"`
 }
