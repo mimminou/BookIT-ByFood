9e02d32a7a16d50589efa283796d069caaa7d02b
mimminou <mimminou@gmail.com>
2024-04-24 09:29:03 +0100

    restored writing logs to file

--- back/server/middleware.go
+++ back/server/middleware.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 //Middleware comes here
@@ -19,21 +21,45 @@ func Cors(next http.Handler) http.Handler {
 }
 
 // Logging Middlware, writes requests to console
+// write to in.log
 func Logging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		logMsg := fmt.Sprintf("Request : %s %s %s", r.RemoteAddr, r.Method, r.URL.Path)
+		logMsg := fmt.Sprintf("%s Request : %s %s %s", time.Now().Format("2006-01-02 15:04:05"), r.RemoteAddr, r.Method, r.URL.Path)
 
-		log.Println(logMsg)
+		//write on disk for persistant logs
+		f, err := os.OpenFile("in.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Println(err)
+		}
+		defer f.Close()
+		if _, err := f.WriteString(logMsg + "\n"); err != nil {
+			log.Println(err)
+		}
+
+		fmt.Println(logMsg)
 		next.ServeHTTP(w, r)
 	})
 }
 
 // ResponseLogging Middlware, writes responses to console
+// write to out.log
 func ResponseLogging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		wr := &myResponseWriter{w, bytes.Buffer{}, http.StatusOK} //Status ok as default
+		logMsg := fmt.Sprintf("%s Response : %s %d %s\n", time.Now().Format("2006-01-02 15:04:05"), r.RemoteAddr, wr.statusCode, wr.body.String())
+
+		//write on disk for persistant logs
+		f, err := os.OpenFile("out.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Println(err)
+		}
+		defer f.Close()
+		if _, err := f.WriteString(logMsg); err != nil {
+			log.Println(err)
+		}
+
+		fmt.Println(logMsg)
 		next.ServeHTTP(wr, r)
-		log.Printf("Response : %s %d %s\n", r.RemoteAddr, wr.statusCode, wr.body.String())
 	})
 }
 
